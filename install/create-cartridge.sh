6c3e5cfd25681e13bdf92b82b7c0d0cba0100b0f
ste <stefano.marina@ymail.com>
2024-10-16 07:22:44 +0200

    updated

--- install/create-cartridge.sh
+++ install/create-cartridge.sh
@@ -12,9 +12,8 @@ else
 fi
 
 mkdir $1
-cp -a ../default_cartridge/* $1/
+cp -r ../default_cartridge/* $1/
 sed "s#cartridge_dir\"\s*:\s*\"[^\"]\+\"#cartridge_dir\":\"${1}\"#" $CURRENT_CFG > $1/config.json
-echo "$CURRENT_CFG updated."
-cp $CURRENT_CFG $1/
-
-echo "a copy of $CURRENT_CFG was made on $1. Zynthomania will look for that when loading."
+echo "created $1/config.json."
+#cp $CURRENT_CFG $1/
+#echo "a copy of $CURRENT_CFG was made on $1. Zynthomania will look for that when loading."
